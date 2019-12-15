4bf830f94d93e6759984f6cdca5fac436dd7764b
osodebailar <andreas.eggen@gmail.com>
2019-12-15 19:37:02 +0100

    new android material bottom navigation bar

---
+++ app/src/main/java/info/nightscout/androidaps/activities/BaseActivity.java
@@ -0,0 +1,18 @@
+package info.nightscout.androidaps.activities;
+
+import android.app.Activity;
+import android.view.View;
+
+
+public class ThemeViewer extends Activity implements View.OnClickListener, View.OnLongClickListener {
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
+    }
+}
