cc612aca17545935e0590b9a3cd9763477a4006e
Andreas <andreas.eggen@gmail.com>
2020-03-01 20:42:46 +0100

    fixed nullpointer on insight battery status

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
@@ -46,14 +46,23 @@ public class StatuslightHandler {
             applyStatuslight("bage", CareportalEvent.PUMPBATTERYCHANGE, batteryView, extended ? (MainApp.getDbHelper().getLastCareportalEvent(CareportalEvent.PUMPBATTERYCHANGE).age(true) + " ") : "", 240, 504);
         } else {
            // all other pumps
-           double batteryLevel = pump.isInitialized() ? pump.getBatteryLevel() : -1;
-           applyStatuslightLevel( R.string.key_statuslights_bat_critical, 30.0,
+           handleLevel(R.string.key_statuslights_bat_critical, 26.0,
                    R.string.key_statuslights_bat_warning, 51.0,
-                   batteryView, "", batteryLevel);
-           batteryView.setText(extended ? (DecimalFormatter.to0Decimal(batteryLevel) + "%  ") : "");
+                   batteryView, "BAT ", pump.getBatteryLevel());
         }
     }
 
+    void handleLevel(int criticalSetting, double criticalDefaultValue,
+                     int warnSetting, double warnDefaultValue,
+                     TextView view, String text, double batteryLevel) {
+        if (view != null) {
+            double resUrgent = SP.getDouble(criticalSetting, criticalDefaultValue);
+            double resWarn = SP.getDouble(warnSetting, warnDefaultValue);
+            view.setText(extended ? (DecimalFormatter.to0Decimal(batteryLevel) + "%  ") : "");
+            SetWarnColor.setColorInverse(view, batteryLevel, resWarn, resUrgent);
+        }
+    }
+    
     void handleAge(String nsSettingPlugin, String eventName, TextView view, String text,
                    int defaultWarnThreshold, int defaultUrgentThreshold) {
         NSSettingsStatus nsSettings = new NSSettingsStatus().getInstance();
