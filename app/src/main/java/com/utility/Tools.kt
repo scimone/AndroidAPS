6218846a42850e25d7c479092cabef2cc31e3b5b
osodebailar <andreas.eggen@gmail.com>
2020-03-15 09:55:17 +0100

    progressbar now better viewable

---
+++ app/src/main/java/com/utility/Tools.kt
@@ -0,0 +1,9 @@
+package com.utility
+
+import android.content.Context
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+
+@ColorInt
+fun Context.getThemeColor(@AttrRes attribute: Int) = TypedValue().let { theme.resolveAttribute(attribute, it, true); it.data }
